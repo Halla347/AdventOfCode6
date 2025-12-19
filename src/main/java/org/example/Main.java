21e3877bad91056239417db99fc0a1f83d200798
Your Name <you@example.com>
2025-12-19 11:48:50 +0100

    Testy poprawka

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Komunikacja komunikacja = new Komunikacja("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw");
         System.out.println(komunikacja.marker());
+
     }
 }
